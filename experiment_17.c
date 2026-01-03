dd120b939952c9a937f2ca723f2c7e89665ef54d
Code-X-ranjeet <ranjeet050k@gmail.com>
2026-01-03 22:49:18 +0530

    added code to all experiment files

---
+++ experiment_17.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2], i, j;
+
+    printf("Enter elements:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("2D Array:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ experiment_5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+    double d = 12.3456;
+
+    printf("Integer: %d\n", a);
+    printf("Float: %.2f\n", b);
+    printf("Character: %c\n", c);
+    printf("Double: %lf\n", d);
+
+    return 0;
+}
