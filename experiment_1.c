89ed14df4b4243027173d552bbb16b3af2182faf
Code-X-ranjeet <ranjeet050k@gmail.com>
2026-01-03 22:10:46 +0530

    added all C practical programs

---
+++ experiment_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+    int n=15;
+    int m=18;
+    int sum=n+m;
+
+    printf("The value of n :-  %d",sum);
+
+}
---
+++ experiment_19.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[20] = "Hello";
+    char s2[20] = "World";
+
+    printf("Length of s1 = %lu\n", strlen(s1));
+
+    strcpy(s2, s1);
+    printf("After strcpy, s2 = %s\n", s2);
+
+    strcat(s1, " C");
+    printf("After strcat, s1 = %s\n", s1);
+
+    if (strcmp(s1, s2) == 0)
+        printf("Strings are equal\n");
+    else
+        printf("Strings are not equal\n");
+
+    return 0;
+}
